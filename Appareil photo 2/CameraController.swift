80cb02e45356fd6a6b4c5179b0410988d9ca0dcb
Matthieu <matthieupasserel@gmail.com>
2018-07-30 09:13:44 +0200

    Mise en place

---
+++ Appareil photo 2/CameraController.swift
@@ -0,0 +1,29 @@
+//
+//  CameraController.swift
+//  Appareil photo 2
+//
+//  Created by Matthieu PASSEREL on 30/07/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import UIKit
+
+class CameraController: UIViewController {
+    
+    @IBOutlet weak var camerVue: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func prendrePhoto(_ sender: Any) {
+    }
+    
+    @IBAction func versLibrary(_ sender: Any) {
+    }
+    
+    @IBAction func rotateCamera(_ sender: Any) {
+    }
+    
+}
